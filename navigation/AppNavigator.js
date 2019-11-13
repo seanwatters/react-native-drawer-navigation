b0c1e0a5f776e2df70e169c6e9b57a7aa6e439ef
github username <sean@watters.io>
2019-11-12 21:45:17 -0700

    commit

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import BottomTabNavigator from './BottomTabNavigator';
+import DrawerNavigator from './DrawerNavigator';
 
 export default createAppContainer(
   createSwitchNavigator({
     // Additional routes such as a login route could
     // be added here:
     // Login: LoginNavigator,
-    Main: BottomTabNavigator
+    Main: DrawerNavigator
   })
 );
